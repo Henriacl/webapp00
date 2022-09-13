ab25ee3d85829ce834316798854b4ab160044120
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 13:09:18 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -8,12 +8,20 @@ st.title("PrePear")
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Site de Receitas do Mackenzie Campinas ")
 
-st.sidebar.title('Receitas')
-paginaselecionada = st.sidebar.selectbox('Receitas', ['Carnes', 'Peixes'])
+st.sidebar.title("Receitas")
+paginaselecionada = st.sidebar.selectbox('Receitas', ["Carnes", "Peixes", "Aves", "Vegetariano", "Vegano"])
 
 if paginaselecionada == 'Carnes': 
   st.title('Receitas com Carne')
-  
+
 elif paginaselecionada == 'Peixes': 
   st.title('com Peixe')
   
+elif paginaselecionada == 'Aves':
+  st.title('Receitas com Carne')
+  
+elif paginaselecionada == 'Vegetariano':
+  st.title('Receitas com Carne')
+    
+elif paginaselecionada == 'Vegano':
+  st.title('Receitas com Carne')
