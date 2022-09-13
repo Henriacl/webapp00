9d76862c887600669da7567e13909791ccb42fa4
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 11:58:11 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,7 +10,9 @@ st.title("PrePear")
 st.header("Cabeçalho")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Site de Receitas do Mackenzie Campinas ")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+
+st.sidebar.title("menu")
