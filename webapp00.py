1a29e6cb50a6671c4a02ec0dd8d581c5ca6e49c2
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 13:37:48 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -13,15 +13,26 @@ if paginaselecionada == "Inicio":
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
-elif paginaselecionada == 'Carnes': 
+elif paginaselecionada == 'Carnes':
+  testeteste = st.selectbox("qual receita", ("carne1", "carne2", "carne3"))
+  if testeteste == "carne1":
+    st.write("receita1")
+  elif testeteste == "carne2":
+    st.write("receita2")
+  elif testeteste == "carne3:
+    st.write("receita3")
+    
   st.title('Receitas com Carne')
-  st.write("[Receitas Com Carne](https://paladar.estadao.com.br/noticias/receita,bife-wellington-como-fazer-o-classico-em-casa-a-prova-de-erro,70003582982)")
-  st.selectbox("opcao")
+  st.write("aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+  Excepteur sint\
+  [Receitas Com Carne](https://paladar.estadao.com.br/noticias/receita,bife-wellington-como-fazer-o-classico-em-casa-a-prova-de-erro,70003582982)")
+ 
 elif paginaselecionada == 'Peixes': 
   st.title('Receitas com Peixe')
   st.write("aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
   Excepteur sint\
   [Leia mais](https://www.sabornamesa.com.br/receitas-de-peixes/file-de-peixe-simples-e-facil)")
+
 elif paginaselecionada == 'Aves':
   st.title('Receitas com Aves')
   
