42d4b0cde2e86f016d035bcdfe26411a882b3852
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 13:21:16 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -7,7 +7,7 @@ paginaselecionada = st.sidebar.selectbox('Receitas', ["Inicio", "Carnes", "Peixe
 
 if paginaselecionada == "Inicio":
   st.title("PrePear")
-  st.subheader("Site de Receitas do Mackenzie Campinas ")
+  st.header("Site de Receitas do Mackenzie Campinas ")
   st.sudheader("quem somos")
   st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
