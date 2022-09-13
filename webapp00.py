64655b4f9434d68299912598efcfefb8c64e30d4
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 12:36:47 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -13,9 +13,9 @@ st.subheader("Site de Receitas do Mackenzie Campinas ")
 st.sidebar.selectbox("Receitas", ["Carnes", "Peixes", "Aves", "Vegetariano", "Vegano"])
 st.sidebar.selectbox("Trends", ["Tiktok", "Instagram", "FaceBook"])
 
-if st.sidebar.selectbox("Receitas") == "Carnes":
+if selectedoption == "Carnes":
   st.title("Receitas com Carne")
   st.selectbox("opçao", ["opr1", "opt2"])
-elif st.sidebar.selectbox("Receitas") == "Peixes":
+elif selectedoption == "Peixes"
   st.title("Receitas com Peixes")
   st.selectbox("opçao", ["opr1", "opt2"])
