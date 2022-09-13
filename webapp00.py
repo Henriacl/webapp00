78215aa8fae1d58c6779a4e9cff25ab784c70e96
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-13 13:23:43 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -9,7 +9,7 @@ if paginaselecionada == "Inicio":
   st.title("PrePear")
   st.header("Site de Receitas do Mackenzie Campinas ")
   st.sudheader("Quem somos")
-  st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+  st.write("Quem somos\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
