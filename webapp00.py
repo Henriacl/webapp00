b69a1cc2b3c7f6c8ff4e0db1d0150bbb79dae5cc
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-09-19 08:16:08 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -24,7 +24,7 @@ elif paginaselecionada == 'Carnes':
     st.image(image)
   elif testeteste == "Carne Seca":
     image = Image.open("imagensnovo/carne seca.jfif")
-    st.write("Receita com Carne Seca n\
+    st.write("Receita com Carne Seca \n
     caldo de mandioca com carne seca n\
     n\
     300g de carne seca \ ")
