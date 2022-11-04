1fc8adf594562bb0584caa0069938ed7bedf7ce4
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-11-04 09:11:51 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -13,13 +13,13 @@ if(conversao == "Inicio"):
     st.header("Site de conversao de PrePear")
 
 elif(conversao == "Gramas"):
-              gramas = 0
-              gramas = st.number_input("Valor em Gramas")
-              xicaras = int(gramas)/250
-              resto = int(gramas) % 250
-              fracao = Fraction(resto , 250)
-              st.text("O Valor em Gramas representa {}.".format(xicaras))
-              st.text("Xicaras e {}.".format(fracao.limit_denominator(5))
-                      
+  gramas = 0
+  gramas = st.number_input("Valor em Gramas")
+  xicaras = int(gramas)/250
+  resto = int(gramas) % 250
+  fracao = Fraction(resto , 250)
+  st.text("O Valor em Gramas representa {}.".format(xicaras))
+  st.text("Xicaras e {}.".format(fracao.limit_denominator(5))
+          
 elif(conversao == "Xicaras"):
               st.text("oioi")
