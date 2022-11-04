9eef5768a4fa576d97c0faa3661146f5415c37fb
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-11-04 09:40:57 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -14,20 +14,6 @@ if(conversao == "Gramas"):
   
 if(conversao == "Xicaras"):
   xicaras = 0 
-  numerador = 0
-  denominador = 1
   xicaras = st.number_input("quantidade de xicaras: ")
-  xicaras = int(xicaras)*250
-  numerador = st.number_input("parte de cima da fração se houver:")
-  denominador = st.number_input("Parte de baixo da fração:")
-
-  if int(denominador) > 0 :
-    fracao = int(numerador)/int(denominador)
-    valor = fracao*250
-    valortotal = valor + xicaras 
-    print("a qtd de xicaras representa", valortotal, "gramas")
-
-  elif int(denominador) <= 0 :
-    valor = int(xicaras)
-    print("a qtd de xicaras representa", int(valor), "gramas")
-
+  valor = int(xicaras)*250
+  st.text("A qtd de xicaras representa {} Gramas.format(valor))
