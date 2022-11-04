f1a87be8a2f8f40ef5c20ab9afb2316a36e71b18
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-11-04 08:31:05 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,19 +3,9 @@
 import streamlit as st
 from PIL import Image
 
-st.sidebar.title("Receitas")
-paginaselecionada = st.sidebar.selectbox('Receitas', ["Inicio", "Carnes", "Peixes", "Aves", "Vegetarianas", "Veganas", "teste"])
+st.selectbox("Qual unidade quer converter: ",
+                ["Gramas", "Xicaras"])
 
-if paginaselecionada == "Inicio":
-  st.title("PrePear")
-  st.header("Site de Receitas do Mackenzie Campinas")
-  st.write("Quem somos: site de receitas do mackenzie campinas feito por henrique, miguel e thalita")
-
-  
-elif paginaselecionada == "teste":
-  st.title("testando")
-  input("valor em gramas")
-  
   
   
 
