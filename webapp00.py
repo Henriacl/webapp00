e63f3d76ceab928ce2d4708a5226a71770f09017
Henriacl <110978304+Henriacl@users.noreply.github.com>
2022-11-04 09:00:13 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,13 +12,13 @@ conversao = st.selectbox("Qual unidade quer converter: ",
 if(conversao == "Inicio"):
     st.header("Site de conversao de PrePear")
 
-elif(conversao == "Gramas"):
-              gramas = 0
-              gramas = st.number_input("Valor em Gramas")
-              xicaras = int(gramas)/250
-              resto = int(gramas) % 250
-              fracao = Fraction(resto , 250)
-              st.text("O Valor em Gramas representa {}.".format(xicaras)."xicaras e {}.".format(fracao.limit_denominator(5))
+#elif(conversao == "Gramas"):
+              #gramas = 0
+              #gramas = st.number_input("Valor em Gramas")
+             # xicaras = int(gramas)/250
+             # resto = int(gramas) % 250
+             # fracao = Fraction(resto , 250)
+             # st.text("O Valor em Gramas representa {}.".format(xicaras)."xicaras e {}.".format(fracao.limit_denominator(5))
   
 
               
